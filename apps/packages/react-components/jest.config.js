74d56a40dba87af972aaf97ce4e25dc5e51c8355
Cody Robards <55755806+CodyRobards@users.noreply.github.com>
2025-09-24 10:05:06 -0500

    chore(jest): map web-components sources in tests

--- apps/packages/react-components/jest.config.js
+++ apps/packages/react-components/jest.config.js
@@ -5,6 +5,11 @@ export default {
     "^.+\\.tsx?$": "ts-jest",
     "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
   },
+  moduleNameMapper: {
+    "^@wavelengthusaf/web-components$": "<rootDir>/../web-components/src/index.ts",
+    "^@wavelengthusaf/web-components/fonts/(.*)$": "<rootDir>/jest/__mocks__/fileMock.js",
+    "^@wavelengthusaf/web-components/(.*)$": "<rootDir>/../web-components/src/$1",
+  },
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   transformIgnorePatterns: ["node_modules"],
   testMatch: ["<rootDir>/jest/**/*.test.ts", "<rootDir>/jest/**/*.test.tsx"],
